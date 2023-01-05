c7a8aa87a0073cbc4105cf65beade9fc3ca3130f
Julio Pampuch <juliopampuch@gmail.com>
2023-01-05 20:05:57 -0300

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TWFNKD5XCY"></Script>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-TWFNKD5XCY');`
+          }}
+        />
+      </Head>
+      < Component {...pageProps} />
+    </>
+  )
 }
