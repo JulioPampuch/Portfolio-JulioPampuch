414d2ae1635b4b357e675bf3b4292849c55b39b0
Julio Pampuch <juliopampuch@gmail.com>
2023-01-05 20:30:54 -0300

    fixing bugs

--- pages/_app.js
+++ pages/_app.js
@@ -4,21 +4,21 @@ import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>  <Head>
-
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-H5X5BT3K8E"></Script>
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `
+    <>
+      <Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-H5X5BT3K8E"></Script>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             
             gtag('config', 'G-TWFNKD5XCY');
             `
-        }}
-      />
-    </Head>
+          }}
+        />
+      </Head>
       < Component {...pageProps} />
     </>
   )
