7a8749a92679b165d7bf28830f4fcf1aa0949e0c
Julio Pampuch <juliopampuch@gmail.com>
2023-03-09 20:02:09 -0300

    fixed footer mobile

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -17,42 +17,9 @@ const StyledFooter = styled.footer`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 15px;
-
 
   p {
     font-size: 14px;
-  }
-
-  img {
-    width: 30px;
-    margin: 0 7.5px;
-
-    border-radius: 50%;
-    cursor: pointer;
-
-    transition: 0.5s;
-  }
-
-  img:hover {
-    scale: 1.1;
-  }
-
-  @media (min-width: 0px) and (max-width: 1023px) {
-    
-    height: 35vh;
-
-    h6 {
-      font-size: 28px;
-    }
-
-    p {
-      font-size: 18px;
-    }
-
-    img {
-      width: 40px;
-    }
   }
 `
 
